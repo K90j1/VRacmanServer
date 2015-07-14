0965d2624ee8856c64e7d7cba8b4cbe8717a5b58
bookair17 <kyoji-exterior@goo.jp>
2015-07-15 02:40:24 +0900

    Update scoring

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -8,10 +8,10 @@ class ScoresController < ApplicationController
 	def show
 		score_num = Score.maximum('score')
 		if score_num.nil?
-			score_num = '%04d' % 0
+			score_num = '%05d' % 0
 		end
 		score = {
-				'score' => '%04d' % score_num,
+				'score' => '%05d' % score_num,
 		}
 		render json: score
   end
@@ -19,13 +19,18 @@ class ScoresController < ApplicationController
   def create
     @score = Score.new(score_params)
 		if @score.save
-			client = Twitter::REST::Client.new do |config|
-				config.consumer_key        = "YOUR_CONSUMER_KEY"
-				config.consumer_secret     = "YOUR_CONSUMER_SECRET"
-				config.access_token        = "YOUR_ACCESS_TOKEN"
-				config.access_token_secret = "YOUR_ACCESS_SECRET"
+			score_num = '%05d' % score_params[:score]
+			status = "@#{score_params[:name]} のスコア #{score_num}"
+			if Score.maximum('score') < score_params[:score].to_i
+				status = " ☆最高得点☆ @#{score_params[:name]} のスコア #{score_num}！！"
 			end
-			client.update("I'm tweeting with @gem!")
+			client = Twitter::REST::Client.new do |config|
+				config.consumer_key        = 'nC9zyaLl0ofz3a3gPLSvRlvgT'
+				config.consumer_secret     = 'rgxlT8xNcpWsxG4b5bI4VBfR5e7yTjnR8yY5pqN00U17ZHdvy7'
+				config.access_token        = '3279813967-7C8CbY97bbJKU81g2UPpK1pAqrOkUAMC8q0YNNq'
+				config.access_token_secret = 'oX2T2R5lRc04wSbSUuaVqcPBaPZ9MrQGwt8i1ZjgSKc8Y'
+			end
+			client.update(status.to_str)
 		else
 			score_params = false
 		end
