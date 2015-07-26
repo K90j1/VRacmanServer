8c42006246399af80200ec46a9236438fa202703
bookair17 <kyoji-exterior@goo.jp>
2015-07-26 19:18:18 +0900

    Update naming

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -1,17 +1,14 @@
 class ScoresController < ApplicationController
+	TAG = '-- VRゲーム VRacMan メイカーフェアトーキョー2015で展示中！！'
 	protect_from_forgery :except => :create
 
-	def index
-		@scores = Score.all
-	end
-
 	def show
-		score_num = Score.maximum('score')
-		if score_num.nil?
-			score_num = '%05d' % 0
+		_score_num = Score.maximum('score')
+		if _score_num.nil?
+			_score_num = '%05d' % 0
 		end
 		score = {
-				'score' => '%05d' % score_num,
+				'score' => '%05d' % _score_num,
 		}
 		render json: score
   end
@@ -19,10 +16,10 @@ class ScoresController < ApplicationController
   def create
     @score = Score.new(score_params)
 		if @score.save
-			score_num = '%05d' % score_params[:score]
-			status = "@#{score_params[:name]} のスコア #{score_num}"
+			_score_num = '%05d' % score_params[:score]
+			_status = "#{score_params[:name]} のスコア #{_score_num} #{TAG}"
 			if Score.maximum('score') < score_params[:score].to_i
-				status = " ☆最高得点☆ @#{score_params[:name]} のスコア #{score_num}！！"
+				_status = " ☆最高得点☆ #{score_params[:name]} のスコア #{_score_num}！！ #{TAG}"
 			end
 			client = Twitter::REST::Client.new do |config|
 				config.consumer_key        = 'nC9zyaLl0ofz3a3gPLSvRlvgT'
@@ -30,11 +27,12 @@ class ScoresController < ApplicationController
 				config.access_token        = '3279813967-7C8CbY97bbJKU81g2UPpK1pAqrOkUAMC8q0YNNq'
 				config.access_token_secret = 'oX2T2R5lRc04wSbSUuaVqcPBaPZ9MrQGwt8i1ZjgSKc8Y'
 			end
-			client.update(status.to_str)
+			client.update(_status.to_str)
+			_result = true
 		else
-			score_params = false
+			_result = false
 		end
-		render json: score_params
+		render json: _result
 	end
 
   private
